47c18b104e4835cb7df7e6439059ea0f32ec4c8d
John DeLong <jdelong@michiganlabs.com>
2023-11-02 08:17:05 -0400

    Attempt to remote Example ThemeSource

--- ThemeSource.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-public protocol ExampleTheme {
-  var TextAccent: Color { get }
-  var TextPrimary: Color { get }
-  var BodyFont: Font { get }
-  var CardRadius: CGFloat { get }
-}
