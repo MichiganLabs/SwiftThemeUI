7d72850aa7ccfde25e5781ccc7f6e5539dcc99e3
John DeLong <jdelong@michiganlabs.com>
2024-10-15 08:26:09 -0400

    Attempt to run plugin only if there are changes

--- Plugins/ThemeBuildTool/ThemeBuildTool.swift
+++ Plugins/ThemeBuildTool/ThemeBuildTool.swift
@@ -2,11 +2,27 @@ import Foundation
 import PackagePlugin
 
 @main
-struct ToomasKitPlugin: BuildToolPlugin {
+struct ThemeBuilderPlugin: BuildToolPlugin {
   func createBuildCommands(context: PackagePlugin.PluginContext, target: PackagePlugin.Target) async throws -> [PackagePlugin.Command] {
 
     let configJSON = target.directory.appending("ThemeConfig.json")
     let generatedFile = context.pluginWorkDirectory.appending("Generated.swift")
+      
+    // Check if the generated file exists and is up-to-date
+    let fileManager = FileManager.default
+    if fileManager.fileExists(atPath: generatedFile.string) {
+      let configAttributes = try fileManager.attributesOfItem(atPath: configJSON.string)
+      let generatedAttributes = try fileManager.attributesOfItem(atPath: generatedFile.string)
+    
+      if
+        let configModificationDate = configAttributes[.modificationDate] as? Date,
+        let generatedModificationDate = generatedAttributes[.modificationDate] as? Date,
+        generatedModificationDate >= configModificationDate
+      {
+        // Generated file is up-to-date, no need to run the build command
+        return []
+      }
+    }
 
     return [
       .buildCommand(
